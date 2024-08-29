99c583211cf3047e6e25190df6831adffb90ab77
Rishika873 <rishupatil0807@gmail.com>
2024-08-29 20:41:58 +0530

    Responsive website done

--- script.js
+++ script.js
@@ -155,3 +155,18 @@ window.addEventListener('DOMContentLoaded', () => {
         followButton.innerText = "FOLLOW";
     }
 }
+
+
+let menu_list_active_button = document.getElementById('menu_list_active_button');
+let menu_side = document.getElementsByClassName('menu_side')[0];
+
+menu_list_active_button.addEventListener('click', ()=> {
+menu_side.style.transform = "unset";
+menu_list_active_button.style.opacity = 0;
+})
+
+let song_side = document.getElementsByClassName('song_side')[0];
+song_side.addEventListener('dblclick', function(){
+    menu_side.style.transform = "translateX(-100%)";
+    menu_list_active_button.style.opacity = 1;
+    })
